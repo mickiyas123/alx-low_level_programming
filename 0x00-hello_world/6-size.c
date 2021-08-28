cea2ce1964746d4cc5e55e17bfcedc8e5d85b0b1
mickiyas123 <mikikinfe345@gmail.com>
2021-08-27 22:08:01 -0700

    Fixed an issue

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,9 +12,9 @@ long int l;
 long long int L;
 float f;
 /* Print statments */
-printf("Size of a char: %lu bytes(s)\n",(unsigned long)sizeof(c));
-printf("Size of an int: %lu bytes(s)\n",(unsigned long)sizeof(i));
-printf("Size of a long int: %lu bytes(s)\n",(signed long)sizeof(l));
-printf("Size of a long long int: %lu bytes(s)\n",(unsigned long)sizeof(L));
-printf("Size of a float: %lu bytes(s)\n",(unsigned long)sizeof(f));
+printf("Size of a char: %lu bytes(s)\n", (unsigned long)sizeof(c));
+printf("Size of an int: %lu bytes(s)\n", (unsigned long)sizeof(i));
+printf("Size of a long int: %lu bytes(s)\n", (unsigned long)sizeof(l));
+printf("Size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(L));
+printf("Size of a float: %lu bytes(s)\n", (unsigned long)sizeof(f));
 }
