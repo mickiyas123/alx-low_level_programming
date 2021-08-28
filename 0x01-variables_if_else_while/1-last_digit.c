fe1515432c51992caf53c69106f3633258f9a975
mickiyas123 <mikikinfe345@gmail.com>
2021-08-28 04:13:18 -0700

    Fixed mixed declarations and code

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,19 +15,19 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	int lastDigit;
-        lastDigit = n % 10;
-        if (lastDigit < 6 && lastDigit !=0)
+	int l;
+        l = n % 10;
+        if (l < 6 && l !=0)
         {
-        printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);     
+        printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);     
         }
-        else if (lastDigit > 5)
+        else if (l > 5)
         {
-        printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
+        printf("Last digit of %d is %d and is greater than 5\n", n, l);
         }
-        else if (lastDigit == 0)
+        else if (l == 0)
         {
-        printf("Last digit of %d is %d and is 0\n", n, lastDigit);
+        printf("Last digit of %d is %d and is 0\n", n, l);
         }
         return (0);
 }
