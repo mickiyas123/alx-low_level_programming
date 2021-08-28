fc4038b68a0eecd77dc30686e600f18c243c2920
mickiyas123 <mikikinfe345@gmail.com>
2021-08-28 04:05:55 -0700

    declared a variable

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,6 +15,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
+	int lastDigit;
         lastDigit = n % 10;
         if (lastDigit < 6 && lastDigit !=0)
         {
