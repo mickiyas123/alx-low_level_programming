66455431ae40912dea19c4381d5454f1e41d8643
mickiyas123 <mikikinfe345@gmail.com>
2021-08-28 04:31:53 -0700

    using betty coding standard

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,16 +11,15 @@
 int main(void)
 {
 	int n;
-       	
+	int l;
+	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	int l;
-	
         l = n % 10;
         if (l < 6 && l !=0)
         {
-        printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);     
+        printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
         }
         else if (l > 5)
         {
