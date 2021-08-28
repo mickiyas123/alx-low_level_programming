1a9ec3fc384c0c3165703c07139eba95966f3163
mickiyas123 <mikikinfe345@gmail.com>
2021-08-28 04:21:42 -0700

    using betty coding standard

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,10 +3,9 @@
 /* more headers goes there */
 #include <stdio.h>
 /* betty style doc for function main goes there */
-/**                                                                                                                                              
- * main - Entry point                                                                                                                            
- *                                                                                                                                               
- * Return: Always 0 (Success)                                                                                                                    
+/**                                                                                                                                              * main - Entry point                                                                                                                           
+ *                                                                                                                                              
+ * Return: Always 0 (Success)                                                                                                                   
  */
 int main(void)
 {
