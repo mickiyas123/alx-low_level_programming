2b66fa74605a3fe8851d06336ff20945a7456606
mickiyas123 <mikikinfe345@gmail.com>
2021-09-11 08:36:15 -0700

    betty style coding

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,22 +8,22 @@
  */
 int main(void)
 {
-        int ch,n;
+	int ch, n;
 
-        for (ch = 48; ch <= 57; ch++)
-        {
-                for (n = ch+1; n <=57; n++)
-                {
-                        putchar(ch);
-                        putchar(n);
+	for (ch = 48; ch <= 57; ch++)
+	{
+		for (n = ch+1; n <= 57; n++)
+		{
+			putchar(ch);
+			putchar(n);
 
-                        if (ch != 56 || (ch == 56 && n != 57))
-                        {
-                                putchar(',');
-                                putchar(' ');
-                        }
-                }
-        }
-        putchar('\n');
-        return (0);
+			if (ch != 56 || (ch == 56 && n != 57))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
