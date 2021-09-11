cefd001e9343110b678e6b5f7eac7b89ff796b05
mickiyas123 <mikikinfe345@gmail.com>
2021-09-11 15:08:35 -0700

    included main.h

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
-#include <stdio.h>
+#include "main.h"
+
 /**
  * main - Entry Point
  * Description: program that prints _putchar
@@ -7,6 +8,14 @@
  */
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
-}	
+}
