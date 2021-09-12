fb31f0baf5498effb35c14f570a938bda6c1a63a
mickiyas123 <mikikinfe345@gmail.com>
2021-09-12 02:21:41 -0700

    Fixed an issue

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,6 +3,8 @@
 /**
  * main - Entry Point
  * Description: prints the alphabet, in lowercase, followed by a new line
+ *
+ * print_alphabet uses the function stated in main.h
  */
 void print_alphabet(void)
 {
