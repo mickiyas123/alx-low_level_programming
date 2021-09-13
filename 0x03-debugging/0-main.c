f4ad82529ab1c96ae7cd6b2e6741f27a887e39a4
mickiyas123 <mikikinfe345@gmail.com>
2021-09-13 08:31:06 -0700

    Betty Style coding

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
-* main - tests function that prints if integer is positive or negative
-* Return: 0
-*/
-
+ * main - Entry Point
+ * Description: tests function that prints if integer is positive or negative
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
         int i;
 
         i = 0;
-
-        positive_or_negative(i);
+	positive_or_negative(i);
 
         return (0);
 }
