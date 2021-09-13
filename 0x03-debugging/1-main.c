0325d637d54a30104c4c0dbbb633d40533e2f807
mickiyas123 <mikikinfe345@gmail.com>
2021-09-13 08:53:20 -0700

    Betty Style coding

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,14 +11,14 @@ int main(void)
 
 	printf("Infinite loop incoming :(\n");
 
-        i = 0;
+	i = 0;
 
-        /* while (i < 10) */
+/* while (i < 10) */
         {
-                putchar(i);
+		putchar(i);
         }
 
-        printf("Infinite loop avoided! \\o/");
+	printf("Infinite loop avoided! \\o/");
 
-        return (0);
+	return (0);
 }
