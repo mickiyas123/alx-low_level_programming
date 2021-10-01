41b31a8d70f980834cf4c0d69f6d4a8574c4f547
mickiyas123 <mikikinfe345@gmail.com>
2021-09-30 18:49:33 -0700

    fixed an issue

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,7 +8,7 @@
  */
 void print_diagonal(int n)
 {
-	if (n < 0)
+	if (n <= 0)
 	{
 		_putchar('\n');
 	}
@@ -28,7 +28,7 @@ void print_diagonal(int n)
 					_putchar(' ');
 				}
 			}
-			_putchar(' ');
+			_putchar('\n');
 		}
 	}
 }
