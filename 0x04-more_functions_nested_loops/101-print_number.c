c9c9d360b39157357de0f82172316cc24831c1b2
mickiyas123 <mikikinfe345@gmail.com>
2021-10-01 02:11:32 -0700

    Fixed an issue

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,17 +10,28 @@ void print_number(int n)
 {
 	if (n < 0)
 	{
-		int k = n * -1;
+		int k;
+
+		k = n * -1;
 
 		if (k > 9)
+		{
+			_putchar(45);
 			_putchar((k / 10) + '0');
-		_putchar((k % 10) + '0');
+			_putchar((k % 10) + '0');
+		}
+		else if (k <= 9)
+		{
+			_putchar(45);
+			_putchar(k +'0');
+
+		}
 	}
 	else
 	{
 		if (n > 9)
-			_putchar((k / 10) + '0');
-		_putchar((k % 10) + '0');
+			_putchar((n / 10) + '0');
+		_putchar((n % 10) + '0');
 	}
 	_putchar('\n');
 }
