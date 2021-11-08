67e2fdb7bc548d26c12c0d3c35b05bbd10ffe258
mickiyas123 <mikikinfe345@gmail.com>
2021-11-08 10:03:08 -0800

    Fixed an issue

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -13,26 +13,16 @@ char *_strchr(char *s, char c)
 	int i;
 	char *position;
 
-	if (c == '\0')
-	{
-		position = 0;
-		break;
-	}
-	else
-	{
-
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] != c)
-		{
-			position = 0;
-		}
 		if (s[i] == c)
 		{
 			position = &s[i];
-			break;
 		}
-	}
+		else
+		{
+			position = 0;
+		}
 	}
 
 	return (position);
