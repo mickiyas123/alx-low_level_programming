80ef88cf90ac07e6685428e7c96bfb3fb62c1657
mickiyas123 <mikikinfe345@gmail.com>
2021-11-10 06:39:09 -0800

    Fixed an issue

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,32 +10,33 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-        int i = 0, j = 0, len = 0;
+	int i = 0, j = 0, len = 0;
 
-        while(needle[len])
-        {
-                len++;
-        }
-        while (haystack[i] && needle[j])
-        {
-                if (haystack[i] == needle[j])
-                {
-                        i++;
-                        j++;
-                }
-                else
-                {
-                        i = i - j + 1;
-                        j = 0;
-                }
-        }
+	while (needle[len])
+	{
+		len++;
+	}
 
-        if (j == len)
-        {
-                return (&haystack[i - j]);
-        }
-        else
-        {
-                return ('\0');
-        }
+	while (haystack[i] && needle[j])
+	{
+		if (haystack[i] == needle[j])
+		{
+			i++;
+			j++
+		}
+		else
+		{
+			i = i - j + 1;
+			j = 0;
+		}
+	}
+	
+	if (j == len)
+	{
+		return (&haystack[i - j]);
+	}
+	else
+	{
+		return ('\0');
+	}
 }
