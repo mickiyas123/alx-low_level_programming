aa36caf8dcb84d963b008bf52c51e7336548d02f
mickiyas123 <mikikinfe345@gmail.com>
2021-11-10 03:19:15 -0800

    function that locates a substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "main"
+
+/**
+ * _strstr - function that locates a substring
+ * @haystack: address of the string to go through
+ * @needle: adress of the string to look for the occuernce in @haystack
+ *
+ * Return: Pointer to the first occernce of @needle in @haystack or NULL
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; needle[i] != '\0'; i++)
+	{
+		for (j = 0; haystack[j] != '\0'; j++)
+		{
+			if (neddle[i] == haystack[j])
+			{
+				return (&haystack[j]);
+				break;
+			}
+		}
+	}
+
+	return ('\0');
+}
