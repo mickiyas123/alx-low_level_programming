3ae75a30d134162b520f8129371214ed8e64f8df
mickiyas123 <mikikinfe345@gmail.com>
2021-11-27 05:44:57 -0800

    Fixed an issue

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,3 +1,6 @@
+#ifndef LISTS_H
+#define LISTS_H
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -12,5 +15,6 @@ typedef struct list_s
     unsigned int len;
     struct list_s *next;
 } list_t;
+size_t print_list(const list_t *h);i
 
-size_t print_list(const list_t *h);
+#endif
