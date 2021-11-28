622467c1b7a8b67062d3ad05466bb51f8460763c
mickiyas123 <mikikinfe345@gmail.com>
2021-11-28 06:37:16 -0800

    function that prints string before main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,12 +2,13 @@
 
 /**
  * before_main - print statment before main
- * 
+ *
  * Return: Always 0
  */
-void before_main() __attribute__((constructor));
+void before_main(void) __attribute__((constructor));
 
-void before_main()
+void before_main(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my"
+                "back!\n");
 }
