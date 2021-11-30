897cac73f9d91f79aa188270224a25b09fee0185
mickiyas123 <mikikinfe345@gmail.com>
2021-11-29 18:57:27 -0800

    Fixed an issue

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+wget https://github.com/mickiyas123/alx-low_level_programming/blob/5e10ff7bcc072f9dcedcbfe45a08551e17c15733/0x18-dynamic_libraries/libpass.so
 LD_PRELOAD="./libpass.so" ./crackpass
