41fa6a80274e76df296b120fdfa5e1f7783d80ba
mickiyas123 <mikikinfe345@gmail.com>
2021-11-30 09:37:51 -0800

    Fixed an issue

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -5,7 +5,7 @@
 main:     mov       rax, 1                  ; system call for write
           mov       rdi, 1                  ; file handle 1 is stdout
           mov       rsi, message            ; address of string to output
-          mov       rdx, 18                 ; number of bytes
+          mov       rdx, 17                 ; number of bytes
           syscall                           ; invoke operating system to do the write
           mov       rax, 60                 ; system call for exit
           xor       rdi, rdi                ; exit code 0
