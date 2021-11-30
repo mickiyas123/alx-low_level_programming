c440a3e444e24420e2eb6f55f3cc92dbc514cf0d
mickiyas123 <mikikinfe345@gmail.com>
2021-11-30 09:35:43 -0800

    Fixed an issue

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,16 @@
-section .text
-        global main
-main:
-        mov edx,len
-        mov ecx, msg
-        mov ebx, 1
-        mov eax, 4
-        int 0x80
 
-        mov eax, 1
-        int 0x80
+          global    main
 
-segment .data
-msg     db "Hello, Holberton", 0xa
-len     equ     $ - msg
+          section   .text
+main:     mov       rax, 1                  ; system call for write
+          mov       rdi, 1                  ; file handle 1 is stdout
+          mov       rsi, message            ; address of string to output
+          mov       rdx, 18                 ; number of bytes
+          syscall                           ; invoke operating system to do the write
+          mov       rax, 60                 ; system call for exit
+          xor       rdi, rdi                ; exit code 0
+          syscall                           ; invoke operating system to exit
+
+          section   .data
+message:  db        "Hello, Holberton", 10      ; note the newline at the end
 
