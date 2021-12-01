7040d554f899ad57b48233b17a4b4aa6185913bd
mickiyas123 <mikikinfe345@gmail.com>
2021-12-01 07:12:15 -0800

    Fixed an issue

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,17 +13,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *temp, *new;
 
-	unsigned int i = 0, unsigned int count = 0;
-
-	while(*head)
-	{
-		*head = (*head)->next;
-		count++;
-	}
-	if (idx > count)
-	{
-		return (*head);
-	}
+	unsigned int i = 0;
 
 	temp = *head;
 
