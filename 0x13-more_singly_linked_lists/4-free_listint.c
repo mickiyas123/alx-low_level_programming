5a236ca01310bbd5dd77ab8ab2a6fa007b67ea51
mickiyas123 <mikikinfe345@gmail.com>
2021-12-01 00:02:58 -0800

    function that frees linked list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint - function that frees a linked list
+ * @head: pointer to the first node
+ *
+ * Return: Always 0.
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(&(head->n));
+		free(head);
+		head = temp->next;
+	}
+}
