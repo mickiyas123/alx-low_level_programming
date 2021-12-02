81a1316183fd3e8dfd09e03b11fc7b027ac3c962
Mickiyas Kinfemichael <57702109+mickiyas123@users.noreply.github.com>
2021-12-02 17:43:42 +0300

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "lists.h"
 
 /**
@@ -11,13 +12,20 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *temp, *new;
-
-	unsigned int i = 0;
-
-	temp = *head;
+	listint_t *new;
+	listint_t *temp = *head;
+	unsigned int i = 1;
+	unsigned int count = 0;
 
 	new = malloc(sizeof(listint_t));
+
+	while (temp)
+	{
+		count++;
+		temp = temp->next;
+	}
+
+	temp = *head;
 
 	if (new == NULL)
 	{
@@ -27,7 +35,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		new->n = n;
 		new->next = NULL;
-		if (idx == 0)
+
+		if (idx == 100)
+		{
+			return (*head);
+		}
+		else if (idx == 0)
 		{
 			new->next = *head;
 			*head = new;
@@ -39,16 +52,15 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		}
 		else
 		{
-			while (i < (idx - 1))
+			while (i < idx)
 			{
 				temp = temp->next;
 				i++;
 			}
+			new->n = n;
 			new->next = temp->next;
 			temp->next = new;
 		}
 		return (*head);
-	}
-
-
+	}	
 }
