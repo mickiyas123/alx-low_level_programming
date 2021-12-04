bf1dfa37576159dcca88957fa6b96fdd0ae8c2b8
mickiyas123 <mikikinfe345@gmail.com>
2021-12-04 07:44:31 -0800

    Fixed an issue

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,8 +1,7 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "lists.h"
+
 /**
- * insert_nodeint_at_index - function that inserts a new node at a given position
+ * insert_nodeint_at_index - inserts a new node at a given position
  * @head: adress of the first node
  * @idx: index of the list where the new node should be added
  * @n: value to be added at given index
@@ -12,44 +11,35 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new, *temp = *head;
-
-	unsigned int count = 0, i = 0;
-
-	while (temp)
-	{
-		count++;
-		temp = temp->next;
-	}
-	temp = *head;
+	unsigned int i;
 
 	new = malloc(sizeof(listint_t));
 
-	if (new == NULL || idx > count)
+	if (new == NULL || *head == NULL)
 		return (NULL);
-	else
+
+	new->n = n;
+	new->next = NULL;
+
+	if (idx == 0)
 	{
-		new->n = n;
-		new->next = NULL;
-		if (idx == 0)
-		{
-			new->next = (*head)->next;
-			*head = new;
-		}
-		else if (idx == 1)
-		{
-			new->next = temp->next;
-			temp->next = new;
-		}
-		else
-		{
-			while (i < (idx - 1))
-			{
-				temp = temp -> next;
-				i++;
-			}
-			new->next = temp->next;
-			temp->next = new;
-		}
+		new->next = *head;
+		*head = new;
+		return (new);
 	}
-	return (*head);
+
+	for (i = 0; i < (idx - 1); i++)
+	{
+		if (temp != NULL)
+			temp = temp->next;
+	}
+
+	if (temp != NULL)
+	{
+		new->next = temp->next;
+		temp->next = new;
+		return (new);
+	}
+	else
+		return (NULL);
 }
