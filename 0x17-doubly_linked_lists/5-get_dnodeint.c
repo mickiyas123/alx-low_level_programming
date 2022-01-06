c5eeaaa894cb1f2db0ea2909f8955e8bb584e3e7
mickiy123 <mikikinfe345@gmail.com>
2022-01-06 13:01:18 +0300

    Fixed an issue

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -4,23 +4,22 @@
 /**
  * get_dnodeint_at_index - gets the nth node of a Doble Linkedlist
  * @head: Address of the first node
- * index: the node index to be retrieved
- * 
+ * @index: the node index to be retrieved
+ *
  * Return: the nth node
  */
+
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-        unsigned int count = 0;
+	unsigned int count = 0;
 
-        while (head)
-        {
-                if (count == index)
-                {
-                        return (head);
-                }
-                count++;
-                head = head->next;
-        }
-        return (NULL);
+	while (head)
+	{
+		if (count == index)
+			return (head);
+		count++;
+		head = head->next;
+	}
+	return (NULL);
 }
 
