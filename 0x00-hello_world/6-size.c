d06da45f1e81c908a854f0032038643647faa166
mickiy123 <mikikinfe345@gmail.com>
2022-06-11 15:48:10 +0300

    Style: use better betty code format

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,17 +5,17 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{/*Variables */
-char c;
-int i;
-long int l;
-long long int L;
-float f;
-/* Print statments */
-printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
-printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
-printf("Size of a long int: %zu byte(s)\n", (unsigned long)sizeof(l));
-printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(L));
-printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-return (0);
+{
+	char c;
+	int i;
+	long int l;
+	long long int L;
+	float f;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+	printf("Size of a long int: %zu byte(s)\n", (unsigned long)sizeof(l));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(L));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	return (0);
 }
