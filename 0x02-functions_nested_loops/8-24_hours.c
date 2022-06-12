04b14ec3f2393b1b7fb83332aa6cdf8b12594c47
mickiy123 <mikikinfe345@gmail.com>
2022-06-12 15:35:54 +0300

    Use for loop

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,12 +8,10 @@ void jack_bauer(void)
 {
 	int i, j;
 
-	i = 0;
 
-	while (i < 24)
+	for (i = 0; i < 24; i++)
 	{
-		j = 0;
-		while (j < 60)
+		for (j = 0; j < 60; j++)
 		{
 			_putchar((i / 10) + '0');
 			_putchar((i % 10) + '0');
@@ -21,8 +19,6 @@ void jack_bauer(void)
 			_putchar((j / 10) + '0');
 			_putchar((j % 10) + '0');
 			_putchar('\n');
-			j++;
 		}
-		i++;
 	}
 }
