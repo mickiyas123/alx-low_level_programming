4354c47636318363f62afab277576ce21fd05aeb
mickiy123 <mikikinfe345@gmail.com>
2022-06-12 13:19:38 +0300

    Refactor code

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,20 +11,19 @@ int main(void)
 
 	for (i = 0; i < 100; i++)
 	{
-		for (j = 0; j < 100; j++)
+		for (j = i + 1; j < 100; j++)
 		{
-			if (i < j)
+			putchar((i / 10) + 48);
+			putchar((i % 10) + 48);
+			putchar(' ');
+
+			putchar((j / 10) + 48);
+			putchar((j % 10) + 48);
+
+			if (i != 98 || j != 99)
 			{
-				putchar((i / 10) + 48);
-				putchar((i % 10) + 48);
+				putchar(',');
 				putchar(' ');
-				putchar((j / 10) + 48);
-				putchar((j % 10) + 48);
-				if (i != 98 || j != 99)
-				{
-					putchar(',');
-					putchar(' ');
-				}
 			}
 
 		}
