4a3b0f9028a245ebc9d41d3b94879d0ea66d5f16
mickiy123 <mikikinfe345@gmail.com>
2022-06-13 16:38:23 +0300

    Refactor code

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the numbers from 1 to 100, followed by a new line
+ * but for multiples of three prints Fizz instead of the number
+ * and for the multiples of five prints Buzz
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if (n % 3 == 0 && n % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (n % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (n % 5 == 0 && n != 100)
+		{
+			printf("Buzz ");
+		}
+		else if (n == 100)
+		{
+			printf("Buzz\n");
+		}
+		else
+		{
+			printf("%d ", n);
+		}
+	}
+	return (0);
+}
