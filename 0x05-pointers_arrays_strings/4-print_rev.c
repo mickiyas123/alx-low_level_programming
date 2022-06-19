f489084f288c570da1004f7509b6dbeb4cbd33a4
mickiy123 <mikikinfe345@gmail.com>
2022-06-19 12:54:17 +0300

    Use for loop

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,9 +10,14 @@ void print_rev(char *s)
 {
 	int i = 0;
 
-	while (s[i])
-		i++;
-	while (i--)
+	for (i = 0; s[i] != '\0'; i++)
+	{
+
+	}
+	for (i = i - 1; s[i] >= 0; i--)
+	{
 		_putchar(s[i]);
+	}
+
 	_putchar('\n');
 }
