01677358a355ec0972f574f429a73b52e2858fd8
mickiy123 <mikikinfe345@gmail.com>
2022-07-08 13:27:35 +0300

    Refactor

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,14 +14,11 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	i = 0;
-	for (n--; n >= 0; n--, i++)
+	for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]);
-		if (n > 0)
-		{
+		if ((n - 1) != i)
 			printf(", ");
-		}
 	}
 	printf("\n");
 }
