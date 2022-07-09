8bb79600ae668e9cffff7ecc54f12ecebf47c99c
mickiy123 <mikikinfe345@gmail.com>
2022-07-09 14:59:55 +0300

    Use for loop

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,11 +13,13 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	while ((*s1 && *s2) && (*s1 == *s2))
+	int i;
+
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
-		s1++;
-		s2++;
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
 	}
 
-	return (*s1 - *s2);
+	return (0);
 }
