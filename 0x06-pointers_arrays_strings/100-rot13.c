1f0f7e1db67d54e82ada3676887d9f1e97249de4
mickiy123 <mikikinfe345@gmail.com>
2022-07-10 15:33:09 +0300

    Use different approach

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,42 +1,45 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * rot13 - Encodes a string using rot13.
+ * @str: String to be encoded
  *
  * Return: A pointer to the encoded string.
  */
 
-/**
- * @s: charcter to be rotated
- */
-
-void calc(char *s);
-
-
 char *rot13(char *str)
 {
-	int i;
+	char alphabet[] = {'A', 'B', 'C', 'D', 'E', 'F',
+			     'G', 'H', 'I', 'J', 'K', 'L',
+			     'M', 'N', 'O', 'P', 'Q', 'R',
+			     'S', 'T', 'U', 'V', 'W', 'X',
+			     'Y', 'Z', 'a', 'b', 'c', 'd',
+			     'e', 'f', 'g', 'h', 'i', 'j',
+			     'k', 'l', 'm', 'n', 'o', 'p',
+			     'q', 'r', 's', 't', 'u', 'v',
+			     'w', 'x', 'y', 'z'};
+	char keys[] = {'N', 'O', 'P', 'Q', 'R', 'S',
+		       'T', 'U', 'V', 'W', 'X', 'Y',
+		       'Z', 'A', 'B', 'C', 'D', 'E',
+		       'F', 'G', 'H', 'I', 'J', 'K',
+		       'L', 'M', 'n', 'o', 'p', 'q',
+		       'r', 's', 't', 'u', 'v', 'w',
+		       'x', 'y', 'z', 'a', 'b', 'c',
+		       'd', 'e', 'f', 'g', 'h', 'i',
+		       'j', 'k', 'l', 'm'};
+	int i, j, len = sizeof(keys) / sizeof(keys[0]);
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		calc(&(str[i]));
+		for (j = 0; j < len; j++)
+		{
+			if (str[i] == alphabet[j])
+			{
+				str[i] = keys[j];
+				break;
+			}
+		}
 	}
 	return (str);
-}
-
-/**
- * calc - calculate rotation of the string.
- * @s: charcter to be rotated
- */
-
-void calc(char *s)
-{
-	if ((*s >= 'A' && *s <= 'M') || (*s >= 'a' && *s <= 'm'))
-	{
-		*s = *s + 13;
-	}
-	else if ((*s >= 'N' && *s <= 'Z') || (*s >= 'n' && *s <= 'z'))
-	{
-		*s = *s - 13;
-	}
 }
