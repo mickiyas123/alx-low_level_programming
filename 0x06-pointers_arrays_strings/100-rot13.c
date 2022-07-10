9d72696cbabb60f1b39b25dbe0b9b1abdb1d7b90
mickiy123 <mikikinfe345@gmail.com>
2022-07-10 15:39:55 +0300

    Update

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -10,29 +10,13 @@
 
 char *rot13(char *str)
 {
-	char alphabet[] = {'A', 'B', 'C', 'D', 'E', 'F',
-			     'G', 'H', 'I', 'J', 'K', 'L',
-			     'M', 'N', 'O', 'P', 'Q', 'R',
-			     'S', 'T', 'U', 'V', 'W', 'X',
-			     'Y', 'Z', 'a', 'b', 'c', 'd',
-			     'e', 'f', 'g', 'h', 'i', 'j',
-			     'k', 'l', 'm', 'n', 'o', 'p',
-			     'q', 'r', 's', 't', 'u', 'v',
-			     'w', 'x', 'y', 'z'};
-	char keys[] = {'N', 'O', 'P', 'Q', 'R', 'S',
-		       'T', 'U', 'V', 'W', 'X', 'Y',
-		       'Z', 'A', 'B', 'C', 'D', 'E',
-		       'F', 'G', 'H', 'I', 'J', 'K',
-		       'L', 'M', 'n', 'o', 'p', 'q',
-		       'r', 's', 't', 'u', 'v', 'w',
-		       'x', 'y', 'z', 'a', 'b', 'c',
-		       'd', 'e', 'f', 'g', 'h', 'i',
-		       'j', 'k', 'l', 'm'};
-	int i, j, len = sizeof(keys) / sizeof(keys[0]);
+	char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char keys[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i, j;
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		for (j = 0; j < len; j++)
+		for (j = 0; alphabet[j] != '\0'; j++)
 		{
 			if (str[i] == alphabet[j])
 			{
